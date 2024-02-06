87c96f82db9490c43da495b302d6c6ae0e50d3bb
AFPedreros <felipe.pedreros94@gmail.com>
2024-02-06 16:48:14 -0500

    Add Next UI theme support and update dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,13 @@ const config = {
   parserOptions: {
     project: true,
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
   extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
   ],
   rules: {
-    // These opinionated rules are enabled in stylistic-type-checked above.
-    // Feel free to reconfigure them to your own preference.
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
 
@@ -25,12 +23,14 @@ const config = {
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/require-await": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/no-misused-promises": [
       "error",
       {
         checksVoidReturn: { attributes: false },
       },
     ],
+    "react-hooks/rules-of-hooks": "off",
   },
 };
 

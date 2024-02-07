a173e2ce7be63232568f56ee0cc7b233dea41b82
AFPedreros <felipe.pedreros94@gmail.com>
2024-02-07 10:18:09 -0500

    Refactor animation variants in template component

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -2,95 +2,20 @@
 import { motion } from "framer-motion";
 
 const variants = {
-  hidden: { opacity: 0, x: -200, y: 0 },
-  enter: { opacity: 1, x: 0, y: 0 },
+  hidden: { opacity: 0, y: 25 },
+  enter: { opacity: 1, y: 0 },
 };
 
-export const perspective = {
-  initial: {
-    scale: 1,
-
-    y: 0,
-  },
-
-  enter: {
-    scale: 1,
-
-    y: 0,
-  },
-
-  exit: {
-    scale: 0.9,
-
-    y: -150,
-
-    opacity: 0.5,
-
-    transition: {
-      duration: 1.2,
-
-      ease: [0.76, 0, 0.24, 1],
-    },
-  },
-};
-
-export const slide = {
-  initial: {
-    y: "100vh",
-  },
-
-  enter: {
-    y: "100vh",
-  },
-
-  exit: {
-    y: 0,
-
-    transition: {
-      duration: 1,
-
-      ease: [0.76, 0, 0.24, 1],
-    },
-  },
-};
-
-export const opacity = {
-  initial: {
-    opacity: 0,
-  },
-
-  enter: {
-    opacity: 1,
-
-    transition: {
-      duration: 0.5,
-    },
-  },
-
-  exit: {
-    opacity: 1,
-  },
-};
-
-const anim = (variants: any) => {
-  return {
-    initial: "initial",
-
-    animate: "enter",
-
-    exit: "exit",
-
-    variants,
-  };
-};
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
-    <div className="inner">
-      <motion.div className="slide" {...anim(slide)} />
-
-      <motion.div className="page" {...anim(perspective)}>
-        <motion.div {...anim(opacity)}>{children}</motion.div>
-      </motion.div>
-    </div>
+    <motion.main
+      variants={variants}
+      initial="hidden"
+      animate="enter"
+      exit="hidden"
+      transition={{ type: "linear", duration: 0.4 }}
+    >
+      {children}
+    </motion.main>
   );
 }

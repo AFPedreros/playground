13e69cc990ca843e3251d9c0d29005d312460741
AFPedreros <felipe.pedreros94@gmail.com>
2024-02-10 11:14:35 -0500

    Remove postRouter and commented out create mutation in tutorialRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,10 +1,8 @@
-import { postRouter } from "@/server/api/routers/post";
 import { topicRouter } from "@/server/api/routers/topic";
 import { tutorialRouter } from "@/server/api/routers/tutorial";
 import { createTRPCRouter } from "@/server/api/trpc";
 
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   topic: topicRouter,
   tutorial: tutorialRouter,
 });

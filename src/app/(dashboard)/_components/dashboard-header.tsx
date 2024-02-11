c6758c1385a9f0658062a33752061c28521ac908
AFPedreros <felipe.pedreros94@gmail.com>
2024-02-10 19:31:57 -0500

    Refactor dashboard header component and remove unused useToggle hook

--- src/app/(dashboard)/_components/dashboard-header.tsx
+++ src/app/(dashboard)/_components/dashboard-header.tsx
@@ -5,12 +5,14 @@ import { Icon } from "@iconify/react";
 import { Button, Tooltip } from "@nextui-org/react";
 import { useSession } from "next-auth/react";
 import { useCallback } from "react";
+import { useMediaQuery } from "usehooks-ts";
 import { AdminLinkButton } from "./admin-link-button";
 import { SearchInput } from "./search-input";
 
 export function DashboardHeader() {
   const isCollapsed = useCollapsedStore((state) => state.isCollapsed);
   const setIsCollapsed = useCollapsedStore((state) => state.setIsCollapsed);
+  const isMobile = useMediaQuery("(max-width: 768px)");
   const { data: session } = useSession();
 
   const user = session?.user;
@@ -20,27 +22,29 @@ export function DashboardHeader() {
   }, [setIsCollapsed]);
 
   return (
-    <div className="mb-6 bg-background w-full">
+    <div className="mb-6 w-full bg-background">
       <header className="flex flex-col gap-y-6 rounded-medium border-small border-divider p-4">
         <div className="flex items-center justify-between">
-          <Tooltip
-            content={isCollapsed ? "Mostrar sidebar" : "Esconder sidebar"}
-            placement="right"
-          >
-            <Button
-              isIconOnly
-              size="sm"
-              variant="flat"
-              color="secondary"
-              onPress={onToggle}
+          {!isMobile && (
+            <Tooltip
+              content={isCollapsed ? "Mostrar sidebar" : "Esconder sidebar"}
+              placement="right"
             >
-              <Icon
-                height={24}
-                icon="solar:sidebar-minimalistic-outline"
-                width={24}
-              />
-            </Button>
-          </Tooltip>
+              <Button
+                isIconOnly
+                size="sm"
+                variant="flat"
+                color="secondary"
+                onPress={onToggle}
+              >
+                <Icon
+                  height={24}
+                  icon="solar:sidebar-minimalistic-outline"
+                  width={24}
+                />
+              </Button>
+            </Tooltip>
+          )}
 
           {user?.role === "ADMIN" && (
             <div className="flex md:hidden">
@@ -52,7 +56,7 @@ export function DashboardHeader() {
           size="sm"
           variant="faded"
           radius="full"
-          className="bg-transparent flex md:hidden"
+          className="flex bg-transparent md:hidden"
         />
       </header>
     </div>

4da7dc0e98835429004337a7f9267b9dc3d426f1
AFPedreros <felipe.pedreros94@gmail.com>
2024-02-16 22:30:25 -0500

    Remove unused code and comments

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,15 +1,8 @@
 import {
   generateReactHelpers,
-  // generateUploadButton,
-  // generateUploadDropzone,
-  // generateUploader,
 } from "@uploadthing/react";
 
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
 
 export const { useUploadThing, uploadFiles } =
   generateReactHelpers<OurFileRouter>();
-
-// export const UploadButton = generateUploadButton<OurFileRouter>();
-// export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
-// export const Uploader = generateUploader<OurFileRouter>();
--- src/server/api/routers/tutorial.ts
+++ src/server/api/routers/tutorial.ts
@@ -1,14 +1,5 @@
 import { createTRPCRouter } from "@/server/api/trpc";
 
 export const tutorialRouter = createTRPCRouter({
-  // create: adminProcedure
-  //   .input(z.object({ name: z.string().min(1) }))
-  //   .mutation(async ({ ctx, input }) => {
-  //     return ctx.db.tutorial.create({
-  //       data: {
-  //         name: input.name,
-  //         createdBy: { connect: { id: ctx.session.user.id } },
-  //       },
-  //     });
-  //   }),
+
 });
